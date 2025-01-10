f321984e78d000d75cdecf74c839aff97e97b93c
ragreon809@gmail.com <ragreon809@gmail.com>
2025-01-10 07:04:58 +0545

    fixed

--- 4_bit_adder.vhd
+++ 4_bit_adder.vhd
@@ -1,8 +1,8 @@
-library IEEE;
+library ieee;
 
-use IEEE.STD_LOGIC_1164.ALL;
-use ieee.STD_LOGIC_AIRTH.all;
-use ieee.STD_LOIGC_UNSIGNED.all;
+use ieee.STD_LOGIC_1164.ALL;
+use ieee.STD_LOGIC_ARITH.all;
+use ieee.STD_LOGCI_UNSIGNED.all;
 
 entity binary_adder_4_bit is
     port (
@@ -16,9 +16,9 @@ end binary_adder_4_bit;
 architecture behavioural of binary_adder_4_bit is
     begin
         process(a, b)
-    variable temp_sum: std_logic_vector (4 downto 0);
+        variable temp_sum: std_logic_vector (4 downto 0);
     begin
-        temp_sum <= ('0' & a) + ('0' & b);
+        temp_sum := ('0' & a) + ('0' & b);
 
         sum <= temp_sum (3 downto 0);
         carry <= temp_sum (4);
